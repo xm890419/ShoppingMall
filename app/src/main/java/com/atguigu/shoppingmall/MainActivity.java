371676cb0bd2bd1ae6c1247a62ea0885641a73a2
xm890419 <506083998@qq.com>
2017-02-22 13:22:15 +0800

    硅谷商城 购物软件

---
+++ app/src/main/java/com/atguigu/shoppingmall/MainActivity.java
@@ -0,0 +1,27 @@
+package com.atguigu.shoppingmall;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fl_main)
+    FrameLayout flMain;
+    @BindView(R.id.rg_main)
+    RadioGroup rgMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //ButterKnife和Activity绑定
+        ButterKnife.bind(this);
+        //默认选中主页面
+        rgMain.check(R.id.rb_home);
+    }
+}
